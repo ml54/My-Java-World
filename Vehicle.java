866d6a931d27d9aafbb4dfe39d5b12da48b69f6b
Manan Lodhia <mananlodhia54@gmail.com>
2019-04-26 17:00:17 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,19 @@
+package project8;
+
+public class Vehicle {
+	String name;
+	String color;
+	int speed;
+	
+	//void print() {
+		//System.out.println(name);
+		//System.out.println(color);
+		//System.out.println(speed);
+	//}
+	Vehicle(String name ,String color , int speed) {
+		System.out.println(name);
+		System.out.println(color);
+		System.out.println(speed);
+	}
+
+}
