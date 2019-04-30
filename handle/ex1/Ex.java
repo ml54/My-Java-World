ce16738a955b77acf8d42d22c7ba236e4a72ccbd
Manan Lodhia <mananlodhia54@gmail.com>
2019-04-30 11:03:44 +0530

    Add files via upload

---
+++ handle/ex1/Ex.java
@@ -0,0 +1,11 @@
+package project12;
+
+public class Ex {
+	int x= 10 ;
+	int y = 15;
+	void foo() throws ArithmeticException{
+		x = x / 0;
+		y = y / 0;
+	}
+
+}
---
+++ handle/ex1/TestEx.java
@@ -0,0 +1,10 @@
+package project12;
+
+public class TestEx {
+	public static void main(String args[]) {
+		Ex e = new Ex();
+		e.foo();
+		
+	}
+
+}
