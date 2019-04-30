144eb4cc6d1acb4724f91d007ad6ec4c68c84ad8
Manan Lodhia <mananlodhia54@gmail.com>
2019-04-30 11:50:56 +0530

    Add files via upload

---
+++ declare/ex1/Ex5.java
@@ -0,0 +1,14 @@
+package project124;
+
+public class Ex5 {
+	int x;
+	int y;
+	int z;
+	void print() {
+		//System.out.println(x);
+		//System.out.println(y);
+		z = x/y;
+		System.out.println("the value of z is :" +z);
+	}
+
+}
