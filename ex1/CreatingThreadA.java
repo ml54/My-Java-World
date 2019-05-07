3bb55140412ebba516f854516f45b089d8ee3410
Manan Lodhia <mananlodhia54@gmail.com>
2019-05-07 10:33:48 +0530

    Add files via upload

---
+++ ex1/CreatingThreadA.java
@@ -0,0 +1,10 @@
+package project15;
+
+public class CreatingThreadA extends Thread{
+	public void run() {
+		for(int i=0;i<=10;i++) 
+		System.out.println(i);
+	}
+
+	
+}
---
+++ ex1/CreatingThreadB.java
@@ -0,0 +1,9 @@
+package project15;
+
+public class CreatingThreadB extends Thread {
+	public void run() {
+		for (int i=0;i<=20;i++)
+		System.out.println(i);
+	}
+
+}
